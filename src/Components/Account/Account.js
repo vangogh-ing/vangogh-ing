509703f8819ebaf812c3948b541073705d740b3d
macabrie <bliang95@gmail.com>
2022-10-16 17:12:25 -0400

    renders org if account has org id

--- src/Components/Account/Account.js
+++ src/Components/Account/Account.js
@@ -1,5 +1,6 @@
-import { useState, useEffect } from "react";
+import { useState, useCallback, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { supabase } from "../../supabaseClient";
 
 import getProfile from "../../Utils/getProfile";
 
@@ -7,10 +8,27 @@ const Account = ({ session }) => {
   const [loading, setLoading] = useState(true);
   const [name, setName] = useState("");
   const [imageUrl, setImageUrl] = useState("");
+  const [userOrgId, setUserOrgId] = useState(null);
+  const [orgName, setOrgName] = useState(null);
+
+  const organizationName = useCallback(async () => {
+    const { data } = await supabase
+      .from("Organization")
+      .select("id, name")
+      .eq("id", userOrgId);
+
+    if (data) {
+      let userOrgName = data[0].name;
+      setOrgName(userOrgName);
+    }
+  }, [userOrgId]);
 
   useEffect(() => {
-    getProfile(session, setLoading, setName, setImageUrl);
-  }, [session]);
+    getProfile(session, setLoading, setName, setImageUrl, setUserOrgId);
+    if (userOrgId) {
+      organizationName();
+    }
+  }, [session, userOrgId, organizationName]);
 
   return (
     <div>
@@ -30,11 +48,20 @@ const Account = ({ session }) => {
               <p>
                 <span>Email:</span> {session.user.email}
               </p>
+              {userOrgId ? (
+                <p className="account_org">
+                  <span>Organization:</span>
+                  {orgName}
+                </p>
+              ) : (
+                <div className="account_org">
+                  <p>No organization linked</p>
+                  <a>create an org?</a>
+                </div>
+              )}
             </div>
             <div className="account_links">
               <Link to="/account/edit">Edit My Profile</Link>
-              <p>No organization linked</p>
-              <a>create an org?</a>
             </div>
           </div>
         </div>
