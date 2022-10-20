565e2fb0896450fc1417bb9a444a25d504a2418e
sonia <soniafaye1@gmail.com>
2022-10-20 15:42:59 -0400

    finished footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -10,44 +10,63 @@ const Footer = () => {
       </div>
 
       <div className="footerInfoBlock">
-        <h3 className="footerTitle"> Contact Us </h3>
+        {/* <h3 className="footerTitle"> Contact Us </h3> */}
         <div className="footerPersonBlock">
-          <p className="footerPerson">Ashley Quevido</p>
-          <a className="footerLink" href="https://github.com/ashleyquevedo/">
-            Github
+          <p className="footerPerson">Ashley Quevedo</p>
+          <a
+            className="footerLink"
+            href="https://github.com/ashleyquevedo/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <img height="25" width="25" src="/GitHub.png" />
           </a>
-          <br />
           <a
             className="footerLink"
             href="https://www.linkedin.com/in/ashleyquevedo/"
+            rel="noreferrer"
+            target="_blank"
           >
-            LinkedIn
+            <img height="25" width="25" src="/linkedIn.png" />
           </a>
         </div>
         <div className="footerPersonBlock">
           <p className="footerPerson">Brianna Liang</p>
-          <a className="footerLink" href="https://github.com/macabrie">
-            Github
+          <a
+            className="footerLink"
+            href="https://github.com/macabrie"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <img height="25" width="25" src="/GitHub.png" />
           </a>
-          <br />
+
           <a
             className="footerLink"
             href="https://www.linkedin.com/in/brianna-liang/"
+            rel="noreferrer"
+            target="_blank"
           >
-            LinkedIn
+            <img height="25" width="25" src="/linkedIn.png" />
           </a>
         </div>
         <div className="footerPersonBlock">
           <p className="footerPerson">Sonia Slobodsky</p>
-          <a className="footerLink" href="https://github.com/soniafaye1">
-            Github
+          <a
+            className="footerLink"
+            href="https://github.com/soniafaye1"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <img height="25" width="25" src="/GitHub.png" />
           </a>
-          <br />
           <a
             className="footerLink"
             href="https://www.linkedin.com/in/sonia-slobodsky"
+            rel="noreferrer"
+            target="_blank"
           >
-            LinkedIn
+            <img height="25" width="25" src="/linkedIn.png" />
           </a>
         </div>
       </div>
