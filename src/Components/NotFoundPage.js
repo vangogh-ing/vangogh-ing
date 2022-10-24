f9abfec5de8cab7a274960883440bd32bfcf2af2
Ashley Quevedo <ashleysquevedo@gmail.com>
2022-10-24 11:06:04 -0400

    404 progress

---
+++ src/Components/NotFoundPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function NotFoundPage(props) {
+  return (
+    <div className="not-found-page">
+      <h1>{props.type ? props.type : "Page"} Not Found!</h1>
+      <img
+        alt="At Eternity's Gate by Van Gogh"
+        src="../../public/notfoundvangogh.jpeg"
+      />
+    </div>
+  );
+}
