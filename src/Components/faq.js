6b94ab52d696875b3918ddab3d07c54a176844e3
sonia <soniafaye1@gmail.com>
2022-10-25 16:05:16 -0400

    messed up css

--- src/Components/faq.js
+++ src/Components/faq.js
@@ -2,6 +2,7 @@ const FAQ = () => {
   return (
     <div id="container">
       <h1>Frequently Asked Questions</h1>
+
       <div className="faqImageDiv">
         <img className="faqImg" src="/vangoghfaq.jpg" />
       </div>
@@ -9,7 +10,7 @@ const FAQ = () => {
       <div className="faqCard">
         <div className="faqBox">
           <h2>WHAT IS VANGOGH-ING?</h2>
-          <p>
+          <p className="faqWords">
             Vangogh-ing is a web application that connects you with art and
             culture events happening around New York City.
           </p>
@@ -17,7 +18,7 @@ const FAQ = () => {
 
         <div className="faqBox">
           <h2>HOW TO USE VANGOGH-ING</h2>
-          <p>
+          <p className="faqWords">
             Make an account! Making an account allows you to follow art
             organizations and add events to your calendar.
           </p>
@@ -25,7 +26,7 @@ const FAQ = () => {
 
         <div className="faqBox">
           <h2>HOW TO FOLLOW ORGANIZATIONS AND EVENTS</h2>
-          <p>
+          <p className="faqWords">
             Find an event and press the Save button, that will bring up a pop up
             that urges you to choose whether you are Interested or Attending an
             event. This choice will be reflected in your Calendar and will allow
@@ -35,7 +36,7 @@ const FAQ = () => {
 
         <div className="faqBox">
           <h2>WHAT IS THE FOR YOU PAGE?</h2>
-          <p>
+          <p className="faqWords">
             The For You page is solely based on the organizations that you
             choose to follow. This allows you to sort through events from
             organizations you have more interest in more easily.
@@ -44,7 +45,7 @@ const FAQ = () => {
 
         <div className="faqBox">
           <h2>HOW DOES THE CALENDAR WORK?</h2>
-          <p>
+          <p className="faqWords">
             The calendar allows you to sort through the events you’ve marked as
             Interested or Attending, giving you a view of what your art
             endeavors could look like.
@@ -53,7 +54,7 @@ const FAQ = () => {
 
         <div className="faqBox">
           <h2>HOW TO MAKE AN ORGANIZATION</h2>
-          <p>
+          <p className="faqWords">
             To make an organization, head to your profile and click the link
             “create an org?”. This will pull up a form for your organization.
             Initial creation only requires a name and an email, the other inputs
@@ -63,7 +64,7 @@ const FAQ = () => {
 
         <div className="faqBox">
           <h2>HOW TO MAKE AN EVENT</h2>
-          <p>
+          <p className="faqWords">
             Now that you have an organization, it’s time to make an event. This
             can be done in Active Events, under the My Org Events tab in the
             navbar. Click on Create Event and fill out the form, once that’s
@@ -76,7 +77,7 @@ const FAQ = () => {
 
         <div className="faqBox">
           <h2>WHAT ARE ACTIVE AND PAST EVENTS?</h2>
-          <p>
+          <p className="faqWords">
             The active events tab is where you will find all your events that
             are currently in action. This is where you can update and delete
             them. The past events tab is for events that have passed - you can't
